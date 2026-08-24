48c9d4dd381d9cafa06909ac7902a35cb6b6c77a
Dhananjay Gurjar <dhananjaygurjar20@gmail.com>
2026-08-24 21:55:02 +0530

    day02

--- numpy-basics.py
+++ numpy-basics.py
@@ -8,18 +8,26 @@ import numpy as np
 
 # Vector , Matrix , Tensor
 
-vector = np.array([1,2,3,4,5])
-print(vector)
+# vector = np.array([1, 2, 3, 4, 5])
+# print(vector)
 
-matrix = np.array([[1,2,3],
-                [4,5]])
-print(matrix) 
+# matrix = np.array([[1, 2, 3], [4, 5]])
+# print(matrix)
 
-tensor = np.array([[1,2,3],
-                   [4,5,6],
-                [7,8]])               
-print(tensor)
+# tensor = np.array([[1, 2, 3], [4, 5, 6], [7, 8]])
+# print(tensor)
 
-a = np.arange(1,22,3)#start , end ,jump
+# a = np.arange(1, 22, 3)  # start , end ,jump
 
-print(a)
+# print(a)
+
+import numpy as np
+data = np.array(
+    [
+        [1, 1200, 1400, 4322, 4500, 4599],  # nanskar
+        [2, 1144, 4585, 5265, 4852, 4585],  # pappe
+        [3, 7852, 4586, 9625, 4852, 3571],  # cholewale
+        [4, 7864, 8569, 4500, 8035, 2345],  # dosaram
+        [5, 7896, 8522, 7931, 8016, 1495],  # hub
+    ]
+)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,12 @@
+vector = np.array([1, 2, 3, 4, 5])
+# print(vector)
+
+# matrix = np.array([[1, 2, 3], [4, 5]])
+# print(matrix)
+
+# tensor = np.array([[1, 2, 3], [4, 5, 6], [7, 8]])
+# print(tensor)
+
+# a = np.arange(1, 22, 3)  # start , end ,jump
+
+# print(a)
